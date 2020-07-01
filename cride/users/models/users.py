b25dfdc171c2371fb3859126bfa4108b2d5ba394
Jesus Milan <jgmc3012@gmail.com>
2020-07-01 00:12:09 -0400

    :star: feat: Profile Model

--- cride/users/models/users.py
+++ cride/users/models/users.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from cride.utils.models import CRideModel
-from .validators import phone_regex
+from cride.users.validators import phone_regex
 
 class User(CRideModel, AbstractUser):
     """User model
